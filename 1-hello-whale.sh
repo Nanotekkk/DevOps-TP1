7ae5b7b0da2af0dedba718d79870d5d5f42d679d
Nanotek <matheo.lancea@gmail.com>
2025-10-20 12:04:41 +0200

    Ajout de l'exercise 1 et 2 du TP1

---
+++ 1-hello-whale.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run a Docker container that says "Hello M1 Cyber 2025" and auto-remove after
+
+docker run --rm anthonyjhoiro/whalesay cowsay "Hello M1 Cyber 2025"
---
+++ 2-python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Start an interactive Python 3 shell in Docker and remove container on exit
+
+docker run -it --rm python:3
